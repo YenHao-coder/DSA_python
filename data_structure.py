1103d89e3b314b4a30aa430bf9814a34aefa387d
Teamo <lgr504123@gmail.com>
2026-01-25 15:19:52 +0800

    利用 Queue 實現 FIFO 邏輯，並搭配 Set 優化查找效率至 $O(1)$

--- data_structure.py
+++ data_structure.py
@@ -13,3 +13,32 @@ queue.append("Customer_1")
 queue.append("Customer_2")
 first_item = queue.popleft()
 print(f"Queue Dequeue: {first_item}")
+
+'''觀看紀錄設計'''
+class VideoHistory:
+    def __init__(self,capacity=10):
+        self.capacity = capacity
+        self.history = deque()
+        self.lookup = set()
+    
+    def watch_video(self,video_id):
+        '''功能:累加觀看紀錄'''
+        if video_id in self.history:
+            self.history.remove(video_id)
+            self.lookup.remove(video_id)
+        if len(self.history) >= self.capacity:
+            oldest = self.history.popleft()
+            self.lookup.remove(oldest)
+        self.history.append(video_id)
+        self.lookup.add(video_id)
+
+        self.read()
+    
+    def read(self):
+        """顯示紀錄"""
+        print(f"目前有{len(self.history)}條紀錄: {list(self.history)}")
+yt = VideoHistory(3)
+yt.watch_video("A")
+yt.watch_video("B")
+yt.watch_video("C")
+yt.watch_video("D")
