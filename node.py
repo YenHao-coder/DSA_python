a8c309e3a4e421013934c78bbbd831808ebfc789
Teamo <lgr504123@gmail.com>
2026-01-25 16:07:32 +0800

    建立 node: 練習 doubly linked list 與增加節點的功能

---
+++ node.py
@@ -0,0 +1,23 @@
+class Node:
+    """節點練習"""
+    def __init__(self, video_id):
+        self.prev = None
+        self.next = None
+        self.video_id = video_id
+
+class VideoTracker:
+    """影片追蹤"""
+    def __init__(self):
+        self.lookup = {}
+        self.head = None
+        self.tail = None
+    
+    def add_new(self, video_id):
+        new_node = Node(video_id)
+        self.lookup[video_id] = new_node
+        if not self.head:
+            self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
