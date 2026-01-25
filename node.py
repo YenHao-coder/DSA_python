13e154fc4fa7eca500149f4c998270c58922ee3c
Teamo <lgr504123@gmail.com>
2026-01-25 17:09:43 +0800

    更新 VideoTrecker: 新紀錄替換舊紀錄功能新增 + 讀取歷史紀錄功能

--- node.py
+++ node.py
@@ -1,3 +1,4 @@
+from collections import deque
 class Node:
     """節點(實例)"""
     def __init__(self, video_id):
@@ -7,13 +8,16 @@ class Node:
 
 class VideoTracker:
     """影片追蹤器"""
-    def __init__(self):
+    def __init__(self,capacity =10):
+        """追蹤器初始化: (capacity:長度, history:歷史, lookup:紀錄索引, head:最新, tail:最舊)"""
         self.lookup = {}
+        self.capacity = capacity
+        self.history = deque()
         self.head = None
         self.tail = None
     
     def add_new(self, video_id):
-        """增加新的紀錄"""
+        """累加新的紀錄"""
         new_node = Node(video_id)
         self.lookup[video_id] = new_node
         if not self.head:
@@ -22,7 +26,8 @@ class VideoTracker:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
-    
+        self.read()
+
     def pin_top(self, video_id):
         """移動到最新紀錄"""
         if video_id not in self.lookup: return
@@ -36,3 +41,29 @@ class VideoTracker:
         node.prev = None
         if self.head: self.head.prev = node
         self.head = node
+    
+    def newest_one_displace(self, video_id):
+        """新節點替換舊節點"""
+        if video_id in self.lookup:
+            self.pin_top(video_id=video_id)
+            return
+        
+        if len(self.lookup) >= self.capacity:
+            old_one = self.tail
+            del self.lookup[old_one.video_id]
+            self.tail = old_one.prev
+            if self.tail: self.tail.next = None
+            else: self.head = None
+        
+        new_node = Node(video_id)
+        self.lookup[video_id] = new_node
+
+        if not self.head: self.head = self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+    
+    def read(self):
+        """讀取歷史紀錄"""
+        print(f"目前有{len(self.history)}條紀錄: {list(self.history)}")
