c722f4dd5c40f4fb72fb25ba577a799a2493b3b1
Teamo <lgr504123@gmail.com>
2026-04-25 13:48:44 +0800

    3.12 語法修正

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from typing import List
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
@@ -6,4 +7,3 @@ class Solution:
         count = Counter(nums)
         k_max = min(k, len(count))
         return heapq.nlargest(k_max, count.keys(), key=count.get)
-        
